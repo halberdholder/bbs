64c721eba1d8b9681cf41abc0485a9969f054dd1
halberdholder <825656416@qq.com>
2019-06-06 17:38:28 +0800

    ckeditor add plugin codesnippet and highlight

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -9,22 +9,31 @@ CKEDITOR.editorConfig = function( config ) {
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
-	];
+	//config.toolbarGroups = [
+	//	{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+	//	{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+	//	{ name: 'links' },
+	//	{ name: 'insert' },
+	//	{ name: 'forms' },
+	//	{ name: 'tools' },
+	//	{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+	//	{ name: 'others' },
+	//	'/',
+	//	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+	//	{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+	//	{ name: 'styles' },
+	//	{ name: 'colors' },
+	//	{ name: 'about' }
+	//];
+
+    //config.extraPlugins = 'codesnippet';
+
+    //config.codeSnippet_languages = {
+    //    javascript: 'JavaScript',
+    //    php: 'PHP'
+    //};
+    
+    config.toolbar = 'Basic';
 
     config.enterMode = CKEDITOR.ENTER_BR; 
     config.shiftEnterMode = CKEDITOR.ENTER_P;
@@ -35,7 +44,7 @@ CKEDITOR.editorConfig = function( config ) {
 	config.removeButtons = 'Underline,Subscript,Superscript';
 
 	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
+	//config.format_tags = 'p;h1;h2;h3;pre';
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
@@ -44,5 +53,15 @@ CKEDITOR.editorConfig = function( config ) {
     config.filebrowserUploadUrl = '/ckeditorUpload?type=File';  
     config.filebrowserImageUploadUrl = "/ckeditorUpload?type=image";
     config.filebrowserUploadMethod = "form";
+
+    config.uiColor = '#f1e4db';
+    config.allowedContent = false; // 是否允许使用源码模式进行编辑
+    config.forcePasteAsPlainText = true; // 是否强制复制过来的文字去除格式
+    config.keystrokes = [
+      [CKEDITOR.CTRL + 86 /* V */, 'paste']
+    ];
+    config.blockedKeystrokes = [
+      CKEDITOR.CTRL + 86
+    ];
 
 };
